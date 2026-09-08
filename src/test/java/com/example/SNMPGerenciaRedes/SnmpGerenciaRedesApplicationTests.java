af089be4c07d46ccb846de10ae78f0e4aeee5e72
Joao <guimajoao72@gmail.com>
2026-09-08 10:06:07 -0400

    Trabalho Gerencia Redes

---
+++ src/test/java/com/example/SNMPGerenciaRedes/SnmpGerenciaRedesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SNMPGerenciaRedes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnmpGerenciaRedesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
