c0691c94d29c170ac65a2be4d7da73b9ced8ff2c
Joao <guimajoao72@gmail.com>
2026-09-08 15:57:35 -0400

    Construção frontend

--- src/main/java/com/example/SNMPGerenciaRedes/controller/ControllerSnmp.java
+++ src/main/java/com/example/SNMPGerenciaRedes/controller/ControllerSnmp.java
@@ -15,7 +15,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-public class ControllerSNMP {
+public class ControllerSnmp {
 
     @GetMapping("/api/dados-snmp")
     public Map<String, String> coletarDadosSnmp(){
